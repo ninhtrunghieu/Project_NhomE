2c59713b0894039aad8072c117ada6cf4d78df48
NgThanhHung04 <nguyenthanhhungg04@gmail.com>
2024-10-23 21:32:15 +0700

    commit file order_admin lan 2

---
+++ NhomE_Shopping/resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@section('content')
+    
+
+    <!-- back top top -->
+    <div class="back-to-top">
+        <a href="#">
+            <i class="fa fa-long-arrow-up"></i>
+        </a>
+    </div>
+    <!-- Page Loader -->
+    <div id="page-preloader">
+        <div class="page-loading">
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+        </div>
+    </div>
+@endsection
---
+++ NhomE_Shopping/resources/views/publics/header.blade.php
@@ -0,0 +1,13 @@
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Centered Search Form -->
+    <form class="form-inline mx-auto"> <!-- mx-auto để căn giữa form -->
+      <div class="input-group input-group-lg"> <!-- input-group-lg để làm form lớn hơn -->
+        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search" style="width: 400px;"> <!-- Điều chỉnh độ rộng -->
+        <div class="input-group-append">
+          <button class="btn btn-navbar" type="submit">
+            <i class="fas fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </form>
+</nav>
