79de9d151c35f8b927ddba7b19d3fec5704f4267
Ngọc Khánh <119680870+ngockhanh29@users.noreply.github.com>
2024-10-23 20:22:05 +0700

    Delete Nhom_E directory

--- Nhom_E/resources/views/publics/footer.blade.php
+++
@@ -1,8 +0,0 @@
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      Vũ Chung Dũng
-    </div>
-    <!-- Default to the left -->
-    <strong>Hệ thống thương mại điện tử </strong>- Đồ nội thất.
-  </footer>
