55da411b1c7dee5f0647710fb3902fd03b8869f2
ninhtrunghieu29 <ninhtrunghieu29@gmail.com>
2024-11-05 07:53:53 +0700

    commit cn DS lan 2

--- NhomE_Shopping/resources/views/dashboard/dashboard.blade.php
+++ NhomE_Shopping/resources/views/dashboard/dashboard.blade.php
@@ -1,88 +1,79 @@
 @extends('layouts.admin')
-
 @section('title')
 <title>Trang quản trị</title>
 @endsection
-
 @section('content')
 <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
     <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
                     <h1 class="m-0 text-dark">Thống kê</h1>
-                </div>
+                </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="/">Trang chủ</a></li>
                         <li class="breadcrumb-item active">Thống kê</li>
                     </ol>
-                </div>
-            </div>
-        </div>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
     </div>
+    <!-- /.content-header -->
+    <!-- Main content -->
     <div class="content">
         <div class="container-fluid">
             <div class="card">
                 <div class="card-header">
                     <h3 class="card-title">Biểu đồ danh số</h3>
-                    <a id="btn-filter" class="btn btn-warning" style="margin-left: 20px;">Lọc</a>
-                    <input type="date" class="form-control" name="start" id="start" style="display:inline;width:200px">
-                    <input type="date" class="form-control" name="end" id="end" style="display:inline;width:200px">
+                    <a style="margin-left: 20px;" id="btn-filter" class="btn btn-warning">Lọc</a>
+                    <input type="date" style="display:inline;width:200px" value="2020-09-20" class="form-control" name="start" id="start">
+                    <input type="date" style="display: inline;width:200px" value="2020-09-30" class="form-control" name="end" id="end">
                 </div>
                 <div class="card-body">
                     <div id="myfirstchart" style="height: 250px;"></div>
                 </div>
+                <!-- /.card-body -->
             </div>
-        </div>
+        </div><!-- /.container-fluid -->
     </div>
+    <!-- /.content -->
 </div>
-
-<!-- Thêm các thư viện Morris.js và jQuery -->
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
-
 <script>
-    $(document).ready(function () {
-        // Khởi tạo biểu đồ rỗng
+    $(document).ready(function(){
         var chart = Morris.Bar({
             element: 'myfirstchart',
-            data: [],
+            data:[],
             xkey: 'period',
-            ykeys: ['order', 'sales', 'profit', 'quantity'],
-            labels: ['Đơn hàng', 'Doanh số', 'Lợi nhuận', 'Số lượng']
+            ykeys: ['order','sales','profit','quantity'],
+            labels: ['Đơn hàng','Doanh số','Lợi nhuận','Số lượng']
         });
-
-        // Xử lý sự kiện khi nhấn nút Lọc
-        $('#btn-filter').on('click', function () {
-            var start = $('#start').val();
-            var end = $('#end').val();
-
-            $.ajax({
-                url: "{{ route('dashboard.get') }}",
-                method: 'POST',
-                data: {
-                    "_token": "{{ csrf_token() }}",
-                    start: start,
-                    end: end
-                },
-                success: function (res) {
-                    if (res.error) {
-                        // Hiển thị thông báo lỗi nếu không có dữ liệu
-                        alert(res.error);
-                        chart.setData([]); // Xóa dữ liệu biểu đồ nếu không có dữ liệu
-                    } else {
-                        // Cập nhật biểu đồ với dữ liệu mới
-                        chart.setData(res);
-                    }
-                },
-                error: function (mess) {
-                    console.log("Lỗi:", mess);
-                }
-            });
-        });
-    });
+        $('#btn-filter').on('click', function() {
+        var start = $('#start').val();
+        var end = $('#end').val();
+        $.ajax({
+            url: "{{url('/dashboard/get')}}",
+            method: 'post',
+            data: {
+                "_token": "{{ csrf_token() }}",
+                start,
+                end
+            },
+            success: function(res) {
+                console.log(res);
+                chart.setData(res);
+            },
+            error: function(mess) {
+                console.log(mess);
+            }
+        })
+    })
+    })
+    
 </script>
 @endsection
--- NhomE_Shopping/routes/web.php
+++ NhomE_Shopping/routes/web.php
@@ -217,10 +217,8 @@ Route::get('/user-order', [CustomerController::class, 'get_order']);
 Route::post('/register-post', [CustomerController::class, 'register_post']);
 
 Route::get('/logout', [CustomerController::class, 'logout']);
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
-Route::post('/dashboard/get', [DashboardController::class, 'get'])->name('dashboard.get');
-
-
+Route::get('/dashboard/index', [DashboardController::class, 'index']);
+Route::post('/dashboard/get', [DashboardController::class, 'get']);
 
 Route::get('/search', [HomeController::class, 'search']);
 
