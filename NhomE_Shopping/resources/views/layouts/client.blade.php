9241c014e444bdfb32b952b27fa432b91853bc44
ninhtrunghieu <166682567+ninhtrunghieu@users.noreply.github.com>
2024-11-07 10:12:30 +0700

    Update client.blade.php

--- NhomE_Shopping/resources/views/layouts/client.blade.php
+++ NhomE_Shopping/resources/views/layouts/client.blade.php
@@ -1,12 +1,8 @@
 <!DOCTYPE html>
-<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
-<!--[if IE 9 ]><html class="ie ie9" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!-->
-<!--<![endif]-->
+
 <html lang="zxx">
 
 
-<!-- Mirrored from demo1.cloodo.com/html/furnitica/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 21 Feb 2021 10:11:34 GMT -->
 <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <!-- Basic Page Needs -->
     
